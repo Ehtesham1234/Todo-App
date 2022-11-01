24af4f7be2afe168aa5a09b0f467580a6b1706ef
Ehtesham1234 <ehteshamusman12344321@gmail.com>
2022-11-01 17:14:20 +0530

    Updates

---
+++ precache-manifest.d64f464b55a0ee992735b3fdf214d3de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4ea8c888bee2999f5be",
+    "url": "/Todo-App/static/css/main.169796a1.chunk.css"
+  },
+  {
+    "revision": "c4ea8c888bee2999f5be",
+    "url": "/Todo-App/static/js/main.c4ea8c88.chunk.js"
+  },
+  {
+    "revision": "e3587a82c9a2818d65df",
+    "url": "/Todo-App/static/js/1.e3587a82.chunk.js"
+  },
+  {
+    "revision": "58db14a1c0e0e44501c3",
+    "url": "/Todo-App/static/js/runtime~main.58db14a1.js"
+  },
+  {
+    "revision": "6d7d9764abcfe9a9f5d73652ba498a51",
+    "url": "/Todo-App/index.html"
+  }
+];
